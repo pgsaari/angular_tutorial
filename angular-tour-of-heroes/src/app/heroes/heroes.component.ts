b964ba4780967a3eff8179a5079e0885bcac4ac4
Paul <pgsaari@outlook.com>
2018-03-15 07:14:00 -0500

    part 7 of angular tutorial - routing

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -11,8 +11,6 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
 
-  selectedHero: Hero;
-
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
@@ -22,9 +20,4 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
 }
